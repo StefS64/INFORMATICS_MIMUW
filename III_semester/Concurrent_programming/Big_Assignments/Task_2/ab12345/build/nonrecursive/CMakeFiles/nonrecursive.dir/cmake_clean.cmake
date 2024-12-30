360437385a8c57fbfc6344a1e1699e8970fa3af5
StefS64 <stefan.swierczewski@gmail.com>
2024-12-31 00:03:22 +0100

    project 2 CP nonrec

---
+++ III_semester/Concurrent_programming/Big_Assignments/Task_2/ab12345/build/nonrecursive/CMakeFiles/nonrecursive.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nonrecursive.dir/main.c.o"
+  "CMakeFiles/nonrecursive.dir/main.c.o.d"
+  "CMakeFiles/nonrecursive.dir/stack.c.o"
+  "CMakeFiles/nonrecursive.dir/stack.c.o.d"
+  "nonrecursive"
+  "nonrecursive.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/nonrecursive.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
